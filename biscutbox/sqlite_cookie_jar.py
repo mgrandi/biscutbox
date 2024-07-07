8dea11afc085fa9cdf3594f1f91fd56dd991a056
Mark Grandi <markgrandi@gmail.com>
2024-07-06 17:22:36 -0700

    have set_cookie just call set_cookies with a single element list

--- biscutbox/sqlite_cookie_jar.py
+++ biscutbox/sqlite_cookie_jar.py
@@ -118,31 +118,7 @@ class SqliteCookieJar(CookieJar):
         :param cookie: the Cookie object to add
         """
 
-        with self._get_sqlite3_database_cursor() as cursor:
-
-            logger.debug("inserting cookie `%s` into database", cookie)
-
-            parameter_dict = {
-                "version": cookie.version,
-                "name": cookie.name,
-                "value": cookie.value,
-                "port": cookie.port,
-                "domain": cookie.domain,
-                "path": cookie.path,
-                "secure": cookie.secure,
-                "expires": cookie.expires,
-                "discard": cookie.discard,
-                "comment": cookie.comment,
-                "comment_url": cookie.comment_url,
-                "rfc2109": cookie.rfc2109,
-                "rest": json.dumps(cookie._rest),
-                "port_specified": cookie.port_specified,
-                "domain_specified": cookie.domain_specified,
-                "domain_initial_dot": cookie.domain_initial_dot,
-                "path_specified": cookie.path_specified
-            }
-
-            cursor.execute(sql_statements.INSERT_COOKIE_STATEMENT, parameter_dict)
+        self.set_cookies(cookie_list=[cookie])
 
 
     def set_cookies(self, cookie_list:list[Cookie]):
