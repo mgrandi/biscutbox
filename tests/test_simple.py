4990977216cf7aa1d0dd6565782790e96e125b23
Mark Grandi <markgrandi@gmail.com>
2024-07-07 16:40:26 -0700

    add a proper test for __len__ / len() we insert a few cookies and verify the count

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -58,3 +58,39 @@ class TestSimple:
 
 
         assert len(sqlite_cookie_jar) >= 0
+
+    def test_insert_rows_and_test_len(
+        self,
+        sqlite_cookie_jar:SqliteCookieJar):
+        '''
+        tests to make sure len() works after creating a database
+        inserting a few rows and then calling len()
+
+        :param sqlite_cookie_jar: a sqlite cookie jar object created with a database
+        saved to a temporary folder somewhere. provided by a fixture
+        '''
+
+        for i in range(5):
+            test_cookie = Cookie(
+                version=0,
+                name=f"a{i}",
+                value=f"b{i}",
+                port=None,
+                port_specified=False,
+                domain="example.com",
+                domain_specified=False,
+                domain_initial_dot=False,
+                path="/",
+                path_specified=True,
+                secure=False,
+                expires=None,
+                discard=True,
+                comment=None,
+                comment_url=None,
+                rest={},
+                rfc2109=False)
+
+            sqlite_cookie_jar.set_cookie(test_cookie)
+
+        assert len(sqlite_cookie_jar) == 5
+
